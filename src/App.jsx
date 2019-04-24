1656fafa74a297c2757639db78ae74a0bc27af5e
Michael Dunkle <michael.dunkle@gmail.com>
2019-04-24 16:48:52 -0600

    Adds loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import loader from './assets/loader.svg';
+
 class App extends Component {
   constructor() {
     super();
@@ -40,59 +42,64 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        {this.state.loading && (
+          <div className="loader">
+            <img src={loader} alt="Loading watershed data" />
+          </div>
+        )}
         {!this.state.loading && (
           <div>
             <h1>River Health</h1>
             <p>An overview of current watershed data in Colorado.</p>
             <div className="row">
-              <div className="col-md-4">
+              <div className="col-xs-12 col-sm-12 col-md-4">
                 <div className="box">
                   <h2 className="box__title">South Platte</h2>
                   <div className="box__channel">
-                    Above Spinney - {this.getChannel('PLASPICO').amount}cfs
+                    <span className="box__display">Above Spinney</span> <span className="box__value">{this.getChannel('PLASPICO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    Dream Stream - {this.getChannel('PLAHARCO').amount}cfs
+                    <span className="box__display">Dream Stream</span> <span className="box__value">{this.getChannel('PLAHARCO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    11 Mile Canyon - {this.getChannel('PLAGEOCO').amount}cfs
+                    <span className="box__display">11 Mile Canyon</span> <span className="box__value">{this.getChannel('PLAGEOCO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    Cheesman Canyon - {this.getChannel('PLACHECO').amount}cfs
+                    <span className="box__display">Cheesman Canyon</span> <span className="box__value">{this.getChannel('PLACHECO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    North Fork Convergence - {this.getChannel('PLASPLCO').amount}cfs
+                    <span className="box__display">North Fork Convergence</span> <span className="box__value">{this.getChannel('PLASPLCO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    Waterton Canyon - {this.getChannel('PLASTRCO').amount}cfs
+                    <span className="box__display">Waterton Canyon</span> <span className="box__value">{this.getChannel('PLASTRCO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    Above Chatfield - {this.getChannel('PLAWATCO').amount}cfs
+                    <span className="box__display">Above Chatfield</span> <span className="box__value">{this.getChannel('PLAWATCO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    Below Chatfield - {this.getChannel('PLACHACO').amount}cfs
+                    <span className="box__display">Below Chatfield</span> <span className="box__value">{this.getChannel('PLACHACO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    DSP - {this.getChannel('PLADENCO').amount}cfs
+                    <span className="box__display">DSP</span> <span className="box__value">{this.getChannel('PLADENCO').amount}cfs</span>
                   </div>
-                  <div>• • •</div>
+                  <div className="box__divider">• • •</div>
                   <div className="box__channel">
-                    Clear Creek - {this.getChannel('CLEGOLCO').amount}cfs
-                  </div>
-                  <div className="box__channel">
-                    North Fork - {this.getChannel('PLABAICO').amount}cfs
+                    <span className="box__display">Clear Creek</span> <span className="box__value">{this.getChannel('CLEGOLCO').amount}cfs</span>
                   </div>
                   <div className="box__channel">
-                    Bear Creek - {this.getChannel('BCRMORCO').amount}cfs
+                    <span className="box__display">North Fork</span> <span className="box__value">{this.getChannel('PLABAICO').amount}cfs</span>
+                  </div>
+                  <div className="box__channel">
+                    <span className="box__display">Bear Creek</span> <span className="box__value">{this.getChannel('BCRMORCO').amount}cfs</span>
                   </div>
                 </div>
               </div>
-              <div className="col-md-4">
+              <div className="col-xs-12 col-sm-12 col-md-4">
                 <div className="box">
                   <h2 className="box__title">Colorado</h2>
                 </div>
               </div>
-              <div className="col-md-4">
+              <div className="col-xs-12 col-sm-12 col-md-4">
                 <div className="box">
                   <h2 className="box__title">Gunnison</h2>
                 </div>
